ed6d6101a0ec3177d9bc9070d1e139257ee87401
xscapintime <louiesxscape@gmail.com>
2021-06-28 17:30:42 +0800

    loop topgo and no scipen

--- cross-species/6-1.topgo-preldefinedist.R
+++ cross-species/6-1.topgo-preldefinedist.R
@@ -2,7 +2,7 @@
 # -------------ORA v1 ---------------
 
 rm(list = ls())
-options(scipen = 999)
+#options(scipen = 999)
 library(tidyverse)
 
 #BiocManager::install("topGO")
@@ -23,13 +23,11 @@ mouse_res <- mouse_res %>% na.omit() ## mouse data has NA
 
 ## gene lists
 # universe lists
-human_res$de <- ifelse(abs(human_res$log2FoldChange) >= 2 &
-                        human_res$padj < 0.01, TRUE, FALSE)
+human_res$de <- ifelse(human_res$padj < 0.01, TRUE, FALSE)
 human_universe <- as.numeric(human_res$de) %>% factor() %>%
         setNames(human_res$row)
 
-mouse_res$de <- ifelse(abs(mouse_res$log2FoldChange) >= 2 &
-                        mouse_res$padj < 0.01, TRUE, FALSE)
+mouse_res$de <- ifelse(mouse_res$padj < 0.01, TRUE, FALSE)
 mouse_universe <- as.numeric(mouse_res$de) %>% factor() %>%
         setNames(mouse_res$row)
 
@@ -61,97 +59,55 @@ m_gene2GO <- unstack(mouse_go_id[, c(2, 1)])
 
 ## topgo object and fisher test
 # BP
-human_GOdata <- new("topGOdata",
-                    ontology = "BP",
+
+go <- c("BP", "MF", "CC")
+
+human_allres <- list()
+
+for (p in go) {
+
+        human_GOdata <- new("topGOdata",
+                    ontology = p,
                     description = "human, biomart, predefined genelist",
                     allGenes = human_universe,
                     annot = annFUN.gene2GO,
-                    gene2GO = h_gene2GO)
+                    gene2GO = h_gene2GO,
+                    nodeSize = 10)
 
-mouse_GOdata <- new("topGOdata",
-                    ontology = "BP",
+        human_res_fisher <- runTest(human_GOdata, algorithm = "classic", statistic = "fisher")
+
+        human_allres[[p]] <- GenTable(human_GOdata, classicFisher = human_res_fisher,
+                                        orderBy = "classicFisher",
+                                        ranksOf = "classicFisher",
+                                        topNodes = length(usedGO(human_GOdata)))
+}
+
+write.table(human_allres[["BP"]], file = "human_predfres_bp.tsv", quote = F, sep = "\t")
+write.table(human_allres[["MF"]], file = "human_predfres_mf.tsv", quote = F, sep = "\t")
+write.table(human_allres[["CC"]], file = "human_predfres_cc.tsv", quote = F, sep = "\t")
+
+
+
+mouse_allres <- list()
+
+for (p in go) {
+
+        mouse_GOdata <- new("topGOdata",
+                    ontology = p,
                     description = "mouse, biomart, predefined genelist",
                     allGenes = mouse_universe,
                     annot = annFUN.gene2GO,
-                    gene2GO = m_gene2GO)
+                    gene2GO = m_gene2GO,
+                    nodeSize = 10)
 
+        mouse_res_fisher <- runTest(mouse_GOdata, algorithm = "classic", statistic = "fisher")
 
-human_res_fisher <- runTest(human_GOdata, algorithm = "classic", statistic = "fisher")
-human_allres_bp <- GenTable(human_GOdata, classicFisher = human_res_fisher,
-                            orderBy = "classicFisher",
-                            ranksOf = "classicFisher",
-                            topNodes = length(usedGO(human_GOdata)))
+        mouse_allres[[p]] <- GenTable(mouse_GOdata, classicFisher = mouse_res_fisher,
+                                        orderBy = "classicFisher",
+                                        ranksOf = "classicFisher",
+                                        topNodes = length(usedGO(mouse_GOdata)))
+}
 
-mouse_res_fisher <- runTest(mouse_GOdata, algorithm = "classic", statistic = "fisher")
-mouse_allres_bp <- GenTable(mouse_GOdata, classicFisher = mouse_res_fisher,
-                            orderBy = "classicFisher",
-                            ranksOf = "classicFisher",
-                            topNodes = length(usedGO(mouse_GOdata)))
-
-write.table(human_allres_bp, file = "human_predfres_bp.tsv", quote = F, sep = "\t")
-write.table(mouse_allres_bp, file = "mouse_predfres_bp.tsv", quote = F, sep = "\t")
-
-
-# MF
-human_GOdata <- new("topGOdata",
-                    ontology = "MF",
-                    description = "human, biomart, predefined genelist",
-                    allGenes = human_universe,
-                    annot = annFUN.gene2GO,
-                    gene2GO = h_gene2GO)
-
-mouse_GOdata <- new("topGOdata",
-                    ontology = "MF",
-                    description = "mouse, biomart, predefined genelist",
-                    allGenes = mouse_universe,
-                    annot = annFUN.gene2GO,
-                    gene2GO = m_gene2GO)
-
-
-human_res_fisher <- runTest(human_GOdata, algorithm = "classic", statistic = "fisher")
-human_allres_mf <- GenTable(human_GOdata, classicFisher = human_res_fisher,
-                            orderBy = "classicFisher",
-                            ranksOf = "classicFisher",
-                            topNodes = length(usedGO(human_GOdata)))
-
-mouse_res_fisher <- runTest(mouse_GOdata, algorithm = "classic", statistic = "fisher")
-mouse_allres_mf <- GenTable(mouse_GOdata, classicFisher = mouse_res_fisher,
-                            orderBy = "classicFisher",
-                            ranksOf = "classicFisher",
-                            topNodes = length(usedGO(mouse_GOdata)))
-
-write.table(human_allres_mf, file = "human_predfres_mf.tsv", quote = F, sep = "\t")
-write.table(mouse_allres_mf, file = "mouse_predfres_mf.tsv", quote = F, sep = "\t")
-
-
-
-# CC
-human_GOdata <- new("topGOdata",
-                    ontology = "CC",
-                    description = "human, biomart, predefined genelist",
-                    allGenes = human_universe,
-                    annot = annFUN.gene2GO,
-                    gene2GO = h_gene2GO)
-
-mouse_GOdata <- new("topGOdata",
-                    ontology = "CC",
-                    description = "mouse, biomart, predefined genelist",
-                    allGenes = mouse_universe,
-                    annot = annFUN.gene2GO,
-                    gene2GO = m_gene2GO)
-
-
-human_res_fisher <- runTest(human_GOdata, algorithm = "classic", statistic = "fisher")
-human_allres_cc <- GenTable(human_GOdata, classicFisher = human_res_fisher,
-                            orderBy = "classicFisher",
-                            ranksOf = "classicFisher",
-                            topNodes = length(usedGO(human_GOdata)))
-
-mouse_res_fisher <- runTest(mouse_GOdata, algorithm = "classic", statistic = "fisher")
-mouse_allres_cc <- GenTable(mouse_GOdata, classicFisher = mouse_res_fisher,
-                            orderBy = "classicFisher",
-                            ranksOf = "classicFisher",
-                            topNodes = length(usedGO(mouse_GOdata)))
-
-write.table(human_allres_cc, file = "human_predfres_cc.tsv", quote = F, sep = "\t")
-write.table(mouse_allres_cc, file = "mouse_predfres_cc.tsv", quote = F, sep = "\t")
+write.table(mouse_allres[["BP"]], file = "mouse_predfres_bp.tsv", quote = F, sep = "\t")
+write.table(mouse_allres[["MF"]], file = "mouse_predfres_mf.tsv", quote = F, sep = "\t")
+write.table(mouse_allres[["CC"]], file = "mouse_predfres_cc.tsv", quote = F, sep = "\t")
