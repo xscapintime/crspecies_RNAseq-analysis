ca6d385be1512c2aaca6259f29dda40eb53451fe
xscapintime <louiesxscape@gmail.com>
2021-08-23 20:31:35 +0800

    more cytotrace

---
+++ human_arrested/cytotrace.v3/noresvt/cytotrace_noresvt.R
@@ -0,0 +1,82 @@
+rm(list = ls())
+
+library(CytoTRACE)
+library(tidyverse)
+
+#setwd(paste0(getwd(), "/arrested"))
+
+#help(package = "CytoTRACE")
+
+## arrested cells only
+expr <- read.table("../data/norm_input-noresveratrol.tsv/norm_input.tsv", sep = "\t", row.names = 1, header = T)
+genes <- row.names(expr)
+stages <- colnames(expr)
+
+expr <- as.data.frame(lapply(expr, as.numeric))
+colnames(expr) <- stages
+rownames(expr) <- genes
+
+## cytotrace
+res <- CytoTRACE(expr)
+save(res, file = "cytotrace_res.Rdata")
+
+#########======================#####################
+load("cytotrace_res.Rdata")
+
+## plot res
+# group the phenotypes
+
+phe <- colnames(expr)
+
+# 1) for E3, E4, E5, E6, E7, split by '.'
+phe <- unlist(lapply(X = colnames(expr), FUN = function(x) {return(strsplit(x, split = ".", fixed = T)[[1]][1])}))
+
+# 2a) for arrested with resvt
+# phe[grep("_res_", phe)] <- unlist(lapply(X = phe[grep("_res_", phe)], FUN = function(x) {
+#     return(strsplit(x, split = "_rp", fixed = F)[[1]][1])}))
+
+# 2b) for arrested 8C, what's ZZU?
+# phe[grep("_8C_", phe)] <- unlist(lapply(X = phe[grep("_8C_", phe)], FUN = function(x) {
+#     return(strsplit(x, split = "_rp", fixed = F)[[1]][1])}))
+
+phe[grep("_8C_", phe)] <- "Hs_8C_arrested"
+
+# 3) for Late_blastocyst
+phe[grep("Late", phe)] <-  unlist(lapply(X = phe[grep("Late", phe)], FUN = function(x) {
+    return(strsplit(x, split = "_E", fixed = F)[[1]][1])}))
+
+# 4) for Epiblast
+phe[grep("Epiblast", phe)] <- "Epiblast"
+
+# 5) for Trophectoderm
+phe[grep("Trophectoderm", phe)] <- "Trophectoderm"
+
+# 6) for Endoderm
+phe[grep("Endoderm", phe)] <- "Endoderm"
+
+# 7) for Morula
+phe[grep("Morula", phe)] <- "Morula"
+
+# 8) for Oocyte
+phe[grep("Oocyte", phe)] <- "Oocyte"
+
+# 9) for Morula
+phe[grep("Zygote", phe)] <- "Zygote"
+
+# 10) for 2C, 4C, 8C
+phe[grep("X", phe)] <- sub("X", "", unlist(lapply(X = phe[grep("X", phe)], FUN = function(x) {
+    return(strsplit(x, split = "_E", fixed = F)[[1]][1])})))
+
+
+# complete grouping!
+names(phe) <- colnames(expr)
+
+
+## cytotrace plot
+plotCytoTRACE(res, phenotype = phe)
+
+
+## plot gene
+plotCytoGenes(res, numOfGenes = 20)
+
+#test <- CytoTRACE(marrow_10x_expr)
