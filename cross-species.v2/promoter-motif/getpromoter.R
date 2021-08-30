d399a9164686163f06a920acf3d4a63ece632af4
xscapintime <louiesxscape@gmail.com>
2021-08-30 15:31:31 +0800

    trimming promoter ranges works

--- cross-species.v2/promoter-motif/getpromoter.R
+++ cross-species.v2/promoter-motif/getpromoter.R
@@ -24,8 +24,8 @@ library(BSgenome.Hsapiens.UCSC.hg38)
 ## human arrested type DEG table
 human_res_t2 <- read.table("../diff-expr/human_deg_t2.tsv")
 
-human_up_t2 <- human_res_t2 %>% filter(log2FoldChange > 2 & padj < 0.05) %>% dplyr::select(row)
-human_dn_t2 <- human_res_t2 %>% filter(log2FoldChange < -2 & padj < 0.05) %>% dplyr::select(row)
+human_up_t2 <- human_res_t2 %>% dplyr::filter(log2FoldChange > 2 & padj < 0.05) %>% dplyr::select(row)
+human_dn_t2 <- human_res_t2 %>% dplyr::filter(log2FoldChange < -2 & padj < 0.05) %>% dplyr::select(row)
 
 
 ### human arrested and mouse diapause common deg ###
@@ -36,7 +36,8 @@ human_dn_t2 <- human_res_t2 %>% filter(log2FoldChange < -2 & padj < 0.05) %>% dp
 
 
 ## match transcripts with gene symbols
-
+## extremely slow way ##
+## ==================================
 system.time({
     up_trs <- GRangesList()
     for(i in 1:nrow(human_up_t2)) {
@@ -59,25 +60,36 @@ system.time({
 names(dn_trs) <- human_dn_t2$row
 
 dn_prom <- getPromoterSeq(dn_trs, Hsapiens, upstream = 1000)
+# so the number is right, but some of them are empty
+# results look weird also, dont't use them
 
 
-## export promoter fasta
-for (i in names(up_prom)) {
+## better way
+## tirm promoter ranges
+## =====================
+up_prom <- promoters(src, upstream = 1000, downstream = 0, filter = SymbolFilter(human_up_t2$row), use.names=TRUE)
+up_prom <- trim(up_prom)
+up_prom <- split(up_prom, up_prom$symbol)
+
+up_prom_seq <- getSeq(Hsapiens, up_prom)
+
+
+dn_prom <- promoters(src, upstream = 1000, downstream = 0, filter = SymbolFilter(human_dn_t2$row), use.names=TRUE)
+dn_prom <- trim(dn_prom)
+dn_prom <- split(dn_prom, dn_prom$symbol)
+
+dn_prom_seq <- getSeq(Hsapiens, dn_prom)
+
+
+## export
+# 624 out of 627
+for (i in names(up_prom_seq)) {
     #writeXStringSet(up_prom[[i]], filepath = paste0("upstream_seq/common_up/", i, "_prom_1k.fasta"), format = "fasta")
-    writeXStringSet(up_prom[[i]], filepath = paste0("upstream_seq/arr_up/", i, "_prom_1k.fasta"), format = "fasta")
+    writeXStringSet(up_prom_seq[[i]], filepath = paste0("upstream_seq/arr_up.v2/", i, "_prom_1k.fasta"), format = "fasta")
 }
 
-for (i in names(dn_prom)) {
+# 888 out of 891
+for (i in names(dn_prom_seq)) {
     #writeXStringSet(dn_prom[[i]], filepath = paste0("upstream_seq/common_dn/", i, "_prom_1k.fasta"), format = "fasta")
-    writeXStringSet(dn_prom[[i]], filepath = paste0("upstream_seq/arr_dn/", i, "_prom_1k.fasta"), format = "fasta")
+    writeXStringSet(dn_prom_seq[[i]], filepath = paste0("upstream_seq/arr_dn.v2/", i, "_prom_1k.fasta"), format = "fasta")
 }
-
-
-
-tmp <- GRangesList()
-    for(i in 1:nrow(human_dn_t2)) {
-        dn_trs[[i]] <- transcripts(src, filter = SymbolFilter(human_dn_t2$row[i]))
-        #dn_trs[[i]] <- dn_trs[[i]][!seqnames(dn_trs[[i]]) %>% stringr::str_detect("_")]
-    }
-
-
---
+++ cross-species.v2/promoter-motif/upstream_seq/arr_dn.v2/upload_data.sh
@@ -0,0 +1,1 @@
+rsync -a ./*.fasta liyang@172.18.4.20:/home/liyang/data3/diapause/arrested_emb/streme.v2/prom_fasta/dngene
