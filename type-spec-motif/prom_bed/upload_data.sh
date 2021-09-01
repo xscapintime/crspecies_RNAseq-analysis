3b63e07b63d8593ed0e843340fea193cfe37d903
xscapintime <louiesxscape@gmail.com>
2021-09-01 21:15:23 +0800

    get promoter seq and coordinates

---
+++ type-spec-motif/prom_bed/upload_data.sh
@@ -0,0 +1,1 @@
+rsync -avP ./*.bed liyang@172.18.4.20:/home/liyang/data3/diapause/arrested_emb/motif_alltype/homer/prom_bed/
---
+++ type-spec-motif/prom_fatsa/upload_data.sh
@@ -0,0 +1,1 @@
+rsync -avP ./*.fasta liyang@172.18.4.20:/home/liyang/data3/diapause/arrested_emb/motif_alltype/streme/prom_fasta/
