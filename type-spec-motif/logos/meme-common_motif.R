7e751b9b1bbf9627f752d4b817d6f2f99f86dffe
xscapintime <louiesxscape@gmail.com>
2021-09-07 11:26:26 +0800

    type-speciic motif

--- type-spec-motif/logos/meme-common_motif.R
+++ type-spec-motif/logos/meme-common_motif.R
@@ -23,20 +23,46 @@ names(tomtom) <- unlist(lapply(X = files, FUN = function(x) {return(strsplit(x, 
 common_up <- Reduce(intersect, list(tomtom[["typeI_up"]]$Target_ID, tomtom[["typeII_up"]]$Target_ID, tomtom[["typeIII_up"]]$Target_ID))
 common_dn <- Reduce(intersect, list(tomtom[["typeI_dn"]]$Target_ID, tomtom[["typeII_dn"]]$Target_ID, tomtom[["typeIII_dn"]]$Target_ID))
 
+## add q.value
+common_up_df <- cbind(tomtom[["typeI_up"]] %>% filter(Target_ID %in% common_up) %>% group_by(Target_ID) %>% summarise(min(q.value)) %>% select(`min(q.value)`),
+                        tomtom[["typeII_up"]] %>% filter(Target_ID %in% common_up) %>% group_by(Target_ID) %>% summarise(min(q.value)) %>% select(`min(q.value)`),
+                        tomtom[["typeIII_up"]] %>% filter(Target_ID %in% common_up) %>% group_by(Target_ID) %>% summarise(min(q.value)) %>% select(`min(q.value)`))
+dimnames(common_up_df) <- list(sort(common_up), names(tomtom)[c(2,4,6)])
+common_up_df <- common_up_df[order(common_up_df),] %>% na.omit
+
+
+
+common_dn_df <- cbind(tomtom[["typeI_dn"]] %>% filter(Target_ID %in% common_dn) %>% group_by(Target_ID) %>% summarise(min(q.value)) %>% select(`min(q.value)`),
+                        tomtom[["typeII_dn"]] %>% filter(Target_ID %in% common_dn) %>% group_by(Target_ID) %>% summarise(min(q.value)) %>% select(`min(q.value)`),
+                        tomtom[["typeIII_dn"]] %>% filter(Target_ID %in% common_dn) %>% group_by(Target_ID) %>% summarise(min(q.value)) %>% select(`min(q.value)`))
+dimnames(common_dn_df) <- list(sort(common_dn), names(tomtom)[c(1,3,5)])
+common_dn_df <- common_dn_df[order(common_dn_df),] %>% na.omit
+
 
 ## export
-write.table(common_up, file = "meme-common_up.txt", quote = F, row.names = F, col.names = F)
-write.table(common_dn, file = "meme-common_dn.txt", quote = F, row.names = F, col.names = F)
+write.table(common_up_df, file = "meme-common_up.txt", quote = F, row.names = T, col.names = T)
+write.table(common_dn_df, file = "meme-common_dn.txt", quote = F, row.names = T, col.names = T)
 
 
 ### specific motif
-typeI_spec_up <-  tomtom[["typeI_up"]] %>% filter(Target_ID %in% setdiff(tomtom[["typeI_up"]]$Target_ID, common_up))
-typeII_spec_up <- setdiff(tomtom[["typeII_up"]]$Target_ID, common_up)
-typeIII_spec_up <- setdiff(tomtom[["typeIII_up"]]$Target_ID, common_up)
+typeI_spec_up <-  tomtom[["typeI_up"]] %>% filter(Target_ID %in% setdiff(tomtom[["typeI_up"]]$Target_ID, common_up)) %>% group_by(Target_ID) %>% summarise(min(q.value))
+typeII_spec_up <- tomtom[["typeII_up"]] %>% filter(Target_ID %in% setdiff(tomtom[["typeII_up"]]$Target_ID, common_up)) %>% group_by(Target_ID) %>% summarise(min(q.value))
+typeIII_spec_up <- tomtom[["typeIII_up"]] %>% filter(Target_ID %in% setdiff(tomtom[["typeIII_up"]]$Target_ID, common_up)) %>% group_by(Target_ID) %>% summarise(min(q.value))
 
-typeI_spec_dn <- setdiff(tomtom[["typeI_dn"]]$Target_ID, common_dn)
-typeII_spec_dn <- setdiff(tomtom[["typeII_dn"]]$Target_ID, common_dn)
-typeIII_spec_dn <- setdiff(tomtom[["typeIII_dn"]]$Target_ID, common_dn)
+# sort by q-val
+typeI_spec_up <- typeI_spec_up[order(typeI_spec_up$`min(q.value)`),] %>% na.omit
+typeII_spec_up <- typeII_spec_up[order(typeII_spec_up$`min(q.value)`),] %>% na.omit
+typeIII_spec_up <- typeIII_spec_up[order(typeIII_spec_up$`min(q.value)`),] %>% na.omit
+
+
+typeI_spec_dn <- tomtom[["typeI_dn"]] %>% filter(Target_ID %in% setdiff(tomtom[["typeI_dn"]]$Target_ID, common_dn)) %>% group_by(Target_ID) %>% summarise(min(q.value))
+typeII_spec_dn <- tomtom[["typeII_dn"]] %>% filter(Target_ID %in% setdiff(tomtom[["typeII_dn"]]$Target_ID, common_dn)) %>% group_by(Target_ID) %>% summarise(min(q.value))
+typeIII_spec_dn <- tomtom[["typeIII_dn"]] %>% filter(Target_ID %in% setdiff(tomtom[["typeIII_dn"]]$Target_ID, common_dn)) %>% group_by(Target_ID) %>% summarise(min(q.value))
+
+# sort by q-val
+typeI_spec_dn <- typeI_spec_dn[order(typeI_spec_dn$`min(q.value)`),] %>% na.omit
+typeII_spec_dn <- typeII_spec_dn[order(typeII_spec_dn$`min(q.value)`),] %>% na.omit
+typeIII_spec_dn <- typeIII_spec_dn[order(typeIII_spec_dn$`min(q.value)`),] %>% na.omit
 
 
 allspec <- list(typeI_spec_up, typeII_spec_up, typeIII_spec_up, typeI_spec_dn, typeII_spec_dn, typeIII_spec_dn)
